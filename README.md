c78ec9fb532de6eb43bea4d61587dab824fe4d1b
Francisco Carlos de Alvarenga <franciscocarlos002@ooutlook.com>
2019-09-10 10:09:44 -0300

    Desenvolvimento do Front-End tela e-Teaching

---
+++ README.md
@@ -0,0 +1,26 @@
+# e-Teaching (eteaching)
+
+Uma plataforma unica e excluisa com foco em professores e alunos, para gerenciamento de atividades acadêmicas.
+
+## Install the dependencies
+```bash
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+### Lint the files
+```bash
+npm run lint
+```
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
---
+++ src/pages/Index.vue
@@ -0,0 +1,68 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-dialog v-model="carousel">
+      <q-carousel
+        transition-prev="slide-right"
+        transition-next="slide-left"
+        swipeable
+        animated
+        v-model="slide"
+        control-color="primary"
+        navigation-icon="radio_button_unchecked"
+        navigation
+        padding
+        height="200px"
+        class="bg-white shadow-1 rounded-borders"
+      >
+        <q-carousel-slide :name="1" class="column no-wrap flex-center">
+          <q-icon name="style" color="primary" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ bemvindo }}
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide :name="2" class="column no-wrap flex-center">
+          <q-icon name="book" color="primary" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ atividade }}
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide :name="3" class="column no-wrap flex-center">
+          <q-icon name="check" color="primary" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ acesso }}
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
+    </q-dialog>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      carousel: false,
+      card: false,
+      sliders: false,
+
+      slide: 1,
+      bemvindo: 'Olá! Seja bem vindo ao sistema educacional e-Teaching! Uma plataforma totalmente resposiva e interativa.',
+      atividade: 'Aqui você terá uma experiencia única com professores, alunos e equipe acaemica para te auxiliarem no que for preciso.',
+      acesso: 'Solicite um acesso agora mesmo a secretaria acadêmica de sua escola e bora pros estudos!',
+
+      stars: 3,
+
+      slideVol: 39,
+      slideAlarm: 56,
+      slideVibration: 63
+    }
+  },
+  mounted () {
+    this.carousel = true
+  }
+}
+</script>
