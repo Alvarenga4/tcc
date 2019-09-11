115a12e2811bc72dd1a3c2f7dababe33bbf93234
Francisco Carlos de Alvarenga <franciscocarlos002@ooutlook.com>
2019-09-10 21:48:02 -0300

    Alteração de tela de login

---
+++ src/layouts/MenuLeft.vue
@@ -0,0 +1,71 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="header">
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+        <img alt="e-Teaching logo" src="~assets/logo.jpg" width="70" height="70">
+        <q-toolbar-title>
+           e-Teaching
+        </q-toolbar-title>
+
+        <div>v {{version}}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <q-item clickable tag="a" target="_blank" href="https://facebook.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="public" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Facebook</q-item-label>
+            <q-item-label caption>@QuasarFramework</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: false,
+      version: '0.0.1'
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style lang="stylus">
+.header {
+  background-color #FFF
+  color #000
+  text-align center
+}
+</style>
